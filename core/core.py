399cbcf865912dbc19b3364e0a25d38a40a49c16
Vzukov Oleg <baNANAmanchic@gmail.com>
2018-10-08 19:07:55 +0300

    1)Recreated methods with steam library \n2)Added more id format support \n3)Minor changes

--- core/core.py
+++ core/core.py
@@ -1,9 +1,14 @@
+import re
 import random
 import asyncio
 import steamMethods
 from discord.ext.commands import Bot
 from keys import discordKey
 
+from datetime import datetime
+
+
+start_time = datetime.now()
 
 BOT_PREFIX = ("!", "?", "1")
 TOKEN = discordKey
@@ -12,45 +17,72 @@ TOKEN = discordKey
 client = Bot(command_prefix=BOT_PREFIX)
 
 
+# idk
+
+def identityFormat(identity):
+    
+    if identity == None:
+        return None
+    
+    elif re.match(r"https://steamcommunity\.com/id/[^/]+", identity) != None:
+        return identity
+
+    elif re.match(r"https://steamcommunity\.com/profiles/\d+", identity) != None:
+        return identity
+
+    elif re.match(r"\d+", identity) != None:
+        return "https://steamcommunity.com/profiles/" + identity
+
+    elif re.match(r"\w+", identity) != None:
+        return "https://steamcommunity.com/id/" + identity
+
+    else:
+        return None
+
+
 # Commands
 
 @client.command(name="mostplayedgames",
-                description="!command steamid add_free_games-optional\n\
+                description="!command profileUrl/id add_free_games-optional\n\
                         Example: !games 76561198115601378 1",
                 brief="Shows user top 10 most played games",
                 aliases=["MostPlayedGames", "PlayedGames", "games"],
                 pass_context=True)
-async def MostPlayedGames(context, steamid=None, free=0):
-    print("%s: !MostPlayedGames %s %i" % (context.message.author, steamid, free))
+async def MostPlayedGames(context, identity=None, free=0):
+    print("%s: !MostPlayedGames %s %i" % (context.message.author, identity, free))
 
-    if steamid == None:
-        await client.say("You did`n specify steamid")
+    identity = identityFormat(identity)
+
+    if identity == None:
+        await client.say("Invalind or on profileUrl/id")
         return 
     
-    res = steamMethods.MostPlayedGames(steamid, free)
+    res = steamMethods.MostPlayedGames(identity, free)
 
     await client.say(context.message.author.mention + "\n" + res)   
 
 
 @client.command(name="getplayerbans",
-                description="!command steamid \n\
+                description="!command profileUrl/id \n\
                         Example: !bans 76561198115601378",
                 brief="Shows user bans info",
                 aliases=["GetPlayerBans", "PlayedBans", "bans"],
                 pass_context=True)
-async def GetPlayerBans(context, steamid=None):
-    print("%s: !GetPlayerBans %s" % (context.message.author, steamid))
+async def GetPlayerBans(context, identity=None):
+    print("%s: !GetPlayerBans %s" % (context.message.author, identity))
 
-    if steamid == None:
-        await client.say("You did`n specify steamid")
+    identity = identityFormat(identity)
+
+    if identity == None:
+        await client.say("Invalind or on profileUrl/id")
         return 
 
-    res = steamMethods.GetPlayerBans(steamid) 
+    res = steamMethods.GetPlayerBans(identity) 
     
     await client.say(context.message.author.mention + "\n" + res)
 
 
-# Вebugging
+# Debugging
 
 @client.event
 async def on_ready():
@@ -58,10 +90,10 @@ async def on_ready():
     print("Logged in as")
     print(client.user.name)
     print(client.user.id)
-    print("----------------")
+    print("----------------\n")
 
 
-async def list_servers():
+async def listServers():
     await client.wait_until_ready()
     await asyncio.sleep(1)
 
@@ -70,11 +102,24 @@ async def list_servers():
         print("Current servers: ")
 
         for server in client.servers:
-            print(server.name, end="\n")
+            print(server.name)
         
-        print("~~~~~~~~~~~~~~~~")
-        await asyncio.sleep(60)
+        print("~~~~~~~~~~~~~~~~\n")
+        
+        await asyncio.sleep(600)
 
 
-client.loop.create_task(list_servers())
+async def upTime():
+    await client.wait_until_ready()
+    await asyncio.sleep(1)
+
+    while not client.is_closed:
+        
+        print("Current up time: {0}".format(datetime.now() - start_time), end="\r")
+
+        await asyncio.sleep(1)
+
+
+client.loop.create_task(listServers())
+client.loop.create_task(upTime())
 client.run(TOKEN)
--- core/steamMethods.py
+++ core/steamMethods.py
@@ -1,9 +1,14 @@
 import requests
+from steam import SteamID
+from steam import WebAPI
 from keys import steamKey
 
 
-STEAM_API_KEY = steamKey
+STEAM_API_KEY = WebAPI(steamKey)
 url = "http://api.steampowered.com/"
+
+def idMapping(identity):
+    pass
 
 
 def getInfo(steamid, method_, payload):    
@@ -12,31 +17,29 @@ def getInfo(steamid, method_, payload):
     return res_json
 
 
-def MostPlayedGames(steamid, free):
-    method_ = "IPlayerService/GetOwnedGames/v0001/"
-    payload = {"key": STEAM_API_KEY, "steamid": steamid, "include_appinfo": "1", "include_played_free_games": free, "format": "json"}
-    
-    res_dict = getInfo(steamid, method_, payload).json()
+def MostPlayedGames(profileUrl, free):
+    steamid = SteamID.from_url(profileUrl)
+
+    res_dict = STEAM_API_KEY.IPlayerService.GetOwnedGames(steamid=steamid, include_appinfo=1, include_played_free_games=free, appids_filter=0)
     res_raw = []
     res = ""
-
+    
     for i in res_dict["response"]["games"]:
         res_raw.append([i["name"], i["playtime_forever"] // 60])
 
     res_raw = sorted(res_raw, key=lambda x:x[1])
-    res_raw.reverse()
-
+    res_raw.reverse()  
+    
     for i in range(10):
         res += "%i) %s: %i hours\n" % (i + 1, res_raw[i][0], res_raw[i][1])
 
     return res
 
 
-def GetPlayerBans(steamid):
-    method_ = "ISteamUser/GetPlayerBans/v1/"
-    payload = {"key": STEAM_API_KEY, "steamids": steamid}
+def GetPlayerBans(profileUrl):
+    steamid = SteamID.from_url(profileUrl)
 
-    res_dict = getInfo(steamid, method_, payload).json()
+    res_dict = STEAM_API_KEY.ISteamUser.GetPlayerBans(steamids=steamid)
     res_raw = []
     res = "Bans info:\n"
 
